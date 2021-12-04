849296dd60adba7dcf886d38438a9cdc94d00f32
ericfib <eric.serrano.lenzetti@estudiantat.upc.edu>
2021-12-04 20:45:33 +0100

    size change done, more or less

--- GroupRun-3D/Assets/Scripts/PlayerMov.cs
+++ GroupRun-3D/Assets/Scripts/PlayerMov.cs
@@ -6,10 +6,14 @@ public class PlayerMov : MonoBehaviour
 {
     public float speedX = 5.0f;
     public float speedZ = 100.0f;
+    public GameObject floor;
+    public int maxChildren = 15;
+
+    private Rigidbody rb;
     private float? lastMousePoint;
     private float startX, floorWidth;
-    public GameObject floor;
-    private Rigidbody rb;
+    private bool hasEvolved;
+    private float timerToEvolve;
 
     // Start is called before the first frame update
     void Start()
@@ -19,34 +23,54 @@ public class PlayerMov : MonoBehaviour
         floorWidth = floor.GetComponent<Renderer>().bounds.size.x - 8f;
         rb = GetComponent<Rigidbody>();
         rb.velocity = new Vector3(0.0f, 0.0f, speedZ);
+        hasEvolved = false;
+        timerToEvolve = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetMouseButtonDown(0))
+        if (transform.childCount < maxChildren)
         {
-            lastMousePoint = Input.mousePosition.x;
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            lastMousePoint = null;
-        }
-        if (lastMousePoint != null)
-        {
-            float difference = Input.mousePosition.x - lastMousePoint.Value;
-
-            bool? fillTocaBorde = null;
-
-            fillTocaBorde = watchChildrenBorders(difference);
-            float newPosx = transform.position.x + (difference * speedX) * Time.deltaTime;
-            if (fillTocaBorde == false)
+            if (Input.GetMouseButtonDown(0))
             {
-                rb.MovePosition(new Vector3(newPosx, transform.position.y, transform.position.z));
+                lastMousePoint = Input.mousePosition.x;
             }
-            lastMousePoint = Input.mousePosition.x;
+            else if (Input.GetMouseButtonUp(0))
+            {
+                lastMousePoint = null;
+            }
+            if (lastMousePoint != null)
+            {
+                float difference = Input.mousePosition.x - lastMousePoint.Value;
+
+                bool? fillTocaBorde = null;
+
+                fillTocaBorde = watchChildrenBorders(difference);
+                float newPosx = transform.position.x + (difference * speedX) * Time.deltaTime;
+                if (fillTocaBorde == false)
+                {
+                    rb.MovePosition(new Vector3(newPosx, transform.position.y, transform.position.z));
+                }
+                lastMousePoint = Input.mousePosition.x;
+            }
+        } else
+        {
+            hasEvolved = true;
+            if (hasEvolved && timerToEvolve >= 0.5)
+            {
+                rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            }
+            else
+            {
+                timerToEvolve += Time.deltaTime;
+
+            }
         }
+
+
+
+
     }
 
     private bool watchChildrenBorders(float difference)
--- GroupRun-3D/Assets/Scripts/multiply_player.cs
+++ GroupRun-3D/Assets/Scripts/multiply_player.cs
@@ -6,15 +6,17 @@ using TMPro;
 public class multiply_player : MonoBehaviour
 {
 
-    public bool isOriginal;
+    public bool isOriginal, needsToEvolve;
     public GameObject cloneObject;
     public Vector2 regionSize = Vector2.one;
+    public int maxChildren = 15;
+    float yVelocity = 0.0f;
+    float smoothTime = 3.0f;
 
-    //private int[,] grid;
-    private int max = 20;
+
+    private float startScale, targetScale;
     private Vector3 offset;
-    private float cellSize, radius;
-    private List<Vector2> points;
+    private float cellSize, radius, timerToTransform;
 
     // Start is called before the first frame update
     void Start()
@@ -23,34 +25,52 @@ public class multiply_player : MonoBehaviour
 
         radius = Mathf.Sqrt((offset.x * offset.x) + (offset.z * offset.z));
         cellSize = radius  / Mathf.Sqrt(2);
-        
+        timerToTransform = 0.0f;
+        startScale = transform.localScale.x;
+        targetScale = startScale * 10.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (needsToEvolve) timerToTransform += Time.deltaTime;
+        if (timerToTransform >= 0.4) destroyChildren();
+        
+        if(gameObject.transform.childCount == 1 && needsToEvolve)
+        {
+            if (transform.localScale.y <= 10.0f)
+            {
+                float newScale = Mathf.SmoothDamp(transform.localScale.x, 10.0f * transform.localScale.x, ref yVelocity, smoothTime);
+                transform.localScale = new Vector3(newScale, newScale, newScale);
+            }
+
+            if (transform.position.y <= 42.1)
+            {
+                float newPosition = Mathf.SmoothDamp(transform.position.y, 42.1f + transform.position.y, ref yVelocity, 0.3f);
+                transform.position = new Vector3 (transform.position.x, newPosition, transform.position.z);
+            }
+        }
        
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("A");
         if (other.gameObject.name.Contains("multiplier_barr"))
         {
             string aux = other.gameObject.GetComponent<TMP_Text>().text;
             int nclones = System.Convert.ToInt32(aux);
             int nchildren = gameObject.transform.childCount;
-
-            if (nchildren + nclones < max)
+            for (int i = 0; i < nclones; i++)
             {
-                    for (int i = 0; i < nclones; i++)
-                    {
-                        Debug.Log("VOY A SPAWNEAR COSAS");
-                        SpawnItem();
-                    }
+                Debug.Log("VOY A SPAWNEAR COSAS");
+                SpawnItem();
             }
         }
+
+        if (transform.childCount >= maxChildren) needsToEvolve = true;
+
     }
     
     private void SpawnItem ()
@@ -81,6 +101,22 @@ public class multiply_player : MonoBehaviour
             {
                 return;
             }
+        }
+    }
+
+    private void destroyChildren()
+    {
+        int nchildren = transform.childCount;
+        for (int i = 1; i < nchildren; i++)
+        {
+            Transform childTF = transform.GetChild(i).transform;
+            Vector3 targetPostition = new Vector3(transform.position.x,
+                                       childTF.position.y,
+                                       transform.position.z);
+            childTF.LookAt(targetPostition);
+            childTF.position = Vector3.MoveTowards(childTF.position, targetPostition, 20 * Time.deltaTime);
+
+            if (childTF.position == targetPostition) Destroy(transform.GetChild(i).gameObject);
         }
     }
    
