4a3863fb48e3188b00e191d925ae4042bfee2e94
ivanlagunas <ivan.lagunas@estudiantat.upc.edu>
2021-12-19 19:03:22 +0100

    level 5

--- GroupRun-3D/Assets/Scripts/BulletMov.cs
+++ GroupRun-3D/Assets/Scripts/BulletMov.cs
@@ -6,15 +6,24 @@ public class BulletMov : MonoBehaviour
 {
     public float speed = 20f;
     private Rigidbody rb;
+    private float aliveTime, lifeTime;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        lifeTime = 10f;
+        aliveTime = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
+        aliveTime += Time.deltaTime;
+        if (aliveTime>=lifeTime)
+        {
+            Destroy(gameObject);
+        }
     }
 }
